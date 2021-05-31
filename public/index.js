065947d18e69caec0334b2fec5d107e66921f64b
Doblepe <Vicbleperez@gmail.com>
2021-05-31 16:15:32 +0200

    To-test-loggin&&register

--- public/index.js
+++ public/index.js
@@ -5,11 +5,40 @@ function showPass() {
 	} else {
 		x.type = 'password';
 	}
-}
+} //FIXME: Conseguir que cambien todos campos.
 function createAcc() {
-	let newacc = {
-		email: document.getElementById('email').value,
-		userName: document.getElementById('username').value,
-		pass: document.getElementById('password').value,
-	};
+	fetch('/registro', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			email: document.getElementById('email').value,
+			userName: document.getElementById('username').value,
+			password: document.getElementById('password').value,
+		}),
+	})
+		.then((res) => res.json())
+		.then(function (datos) {
+			datos.contenido.insertedCount >= 1
+				? (document.getElementById('feedback').innerHTML = '<h3>Cuenta añadida correctamente</h3>')
+				: (document.getElementById('feedback').innerHTML = '<h3>Se ha producido un error</h3>');
+		});
+}
+function loginAcc() {
+	fetch('/registro', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			userName: document.getElementById('loginName').value,
+			password: document.getElementById('loginPassword').value,
+		}),
+	})
+		.then((res) => res.json())
+		.then(function (datos) {
+			datos.contenido.insertedCount >= 1
+				? ((document.getElementById('feedback').innerHTML = '<h3>LOGIN CORRECTO</h3>'),
+				  (document.getElementById('loggedUser').innerHTML = sessionStorage.getItem(userName)))
+				: //TODO: Crear una función que almacene el sesionStorage
+				  //TODO: Devolver el item document.getElementById("result").innerHTML = sessionStorage.getItem("lastname")
+				  (document.getElementById('feedback').innerHTML = '<h3>Se ha producido un error</h3>');
+		});
 }
--- register.js
+++ register.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(mostrarIp); // este App.Use obliga a usar la función intermedia en cada llamada. (modo developer?)
 const cifrarExterno = require('./public/cifrar');
 
-MongoClient.connect('mongodb+srv://vicdoblepe:vicdoblepe2@cards-game.skn15.mongodb.net/test', function (err, client) {
+MongoClient.connect(process.env.MONGO_URL, function (err, client) {
 	//utilizamos las variables de entorno para almacenar nuestra información sensible.
 	err ? console.log(err) : (db = client.db('store'));
 });
@@ -20,59 +20,49 @@ app.post('/registro', cifrarExterno, function (req, res) {
 	//funión intermedia cifrarExtremo del módulo importado
 	let username = req.body.username;
 	let password = req.body.password;
-
-	db.collection('users').insertOne(
-		{ username: username, password: contraseinaCifrada, email: email, bag: [], logged: false },
-		function (err, result) {
-			if (err !== null) {
-				res.send({ mensaje: 'Error al registrar el usuario' });
-			} else {
-				res.send({ mensaje: 'Usuario registrado correctamente' });
-			}
-		}
-	);
-});
-
-app.post('/login', function (req, res) {
-	let username = req.body.username;
-	let password = req.body.password;
-
+	// TODO: Comrpobar si da ERROR por cualquier motivo
 	db.collection('users')
-		.find({ username: username })
+		.find({ username: req.body.username })
 		.toArray(function (err, data) {
-			if (err !== null) {
-				res.send({ mensaje: 'Ha habido un error' });
+			if (err) {
+				res.send({ error: true, contenido: err });
 			} else {
-				if (data.length > 0) {
-					if (bcrypt.compareSync(password, data[0].password)) {
-						res.send(
-							{ mensaje: 'Logueado correctamente' }.UpdateOne({username: req.body.username}, {$set {logged: true}}),
-							// TODO: Modificar el estado de logged LA SINTAXIS ME LA HE INVENTADO. 
-						);
-					} else {
-						res.send({ mensaje: 'Contraseña incorrecta' });
-					}
-				} else {
-					res.send({ mensaje: 'El usuario no existe' });
+				if (data.length >= 0) {
+					db.collection('users').insertOne(
+						{ username: username, password: contraseinaCifrada, email: email, bag: [] },
+						function (err, mensaje) {
+							if (err !== null) {
+								res.send({ mensaje: 'Error al registrar el usuario' });
+							} else {
+								res.send({ mensaje: 'Usuario registrado correctamente' });
+							}
+						}
+					);
 				}
 			}
 		});
+
+	app.post('/login', function (req, res) {
+		let username = req.body.username;
+		let password = req.body.password;
+		db.collection('users')
+			.find({ username: username })
+			.toArray(function (err, data) {
+				if (err !== null) {
+					res.send({ mensaje: 'Ha habido un error' });
+				} else {
+					if (data.length > 0) {
+						if (bcrypt.compareSync(password, data[0].password)) {
+							res.send({ mensaje: 'Logueado correctamente' }, function storage() {
+								sessionStorage.setItem('userName', `${req.body.userName}`);
+							});
+						} else {
+							res.send({ mensaje: 'Contraseña incorrecta' });
+						}
+					} else {
+						res.send({ mensaje: 'El usuario no existe' });
+					}
+				}
+			});
+	});
 });
-
-/* app.post('/registro', function (req, res) {
-	let username = req.body.username;
-	let email = req.body.email;
-	let password = req.body.password;
-	let contraseinaCifrada = bcrypt.hashSync(password, 10);
-
-	db.collection('accounts').insertOne(
-		{ username: username, password: contraseinaCifrada, email: email, bag: [], logged: false },
-		function (err, result) {
-			if (err !== null) {
-				res.send({ mensaje: 'Error al registrar el usuario' });
-			} else {
-				res.send({ mensaje: 'Usuario registrado correctamente' });
-			}
-		}
-	);
-}); */
