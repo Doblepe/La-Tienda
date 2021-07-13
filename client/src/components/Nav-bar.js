51cd321fb5c8661e3dc34e30c5a1bd6d0b07e13d
Doblepe <vicdobleperez@gmail.com>
2021-07-13 10:36:35 +0200

    sesion-to-link

--- client/src/components/Nav-bar.js
+++ client/src/components/Nav-bar.js
@@ -1,26 +1,9 @@
-import { Navbar, Nav, Button,NavDropdown, Popover, OverlayTrigger } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown,} from 'react-bootstrap';
 import logo from '../assets/IMG_8178-min.jpg'
 import {Link} from 'react-router-dom'
-import { useState } from 'react';
 
 
 function NavBarComp(props){
-
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">Inicia tu sesión</Popover.Title>
-      <Popover.Content>
-        <p>Si eres cliente nuestro entonces inicia sesión <Link to="/login">Click aquí</Link></p>
-        <hr/>
-        <p>Si no eres cliente nuestro crea tu cuenta <Link to="/registro">Click aquí</Link></p>
-      </Popover.Content>
-    </Popover>
-  );
-  const MyAcc = () => (
-    <OverlayTrigger trigger="click" placement="left" overlay={popover}>
-      <Button variant="success">Mi cuenta</Button>
-    </OverlayTrigger>
-  );
 
     return(
     <Navbar bg="light" expand="lg">
@@ -35,16 +18,19 @@ function NavBarComp(props){
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="/">Inicio</Nav.Link>
-        <Nav.Link href="/contacto">Contacto</Nav.Link>
+        <Nav.Link  as={Link} to="/">Inicio</Nav.Link>
+        <Nav.Link as={Link} to= "/contacto">Contacto</Nav.Link>
         <NavDropdown title="Productos" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/products"onClick={()=>props.setSelect('products')}>Todos los productos</NavDropdown.Item>
-          <NavDropdown.Item href="/products/femaleCollection" onClick={()=>props.setSelect('femaleCollection')}>Moda femenina</NavDropdown.Item>
-          <NavDropdown.Item href="/products/maleCollection"onClick={()=>props.setSelect('maleCollection')}>Moda Masculina</NavDropdown.Item>
-          <NavDropdown.Item href="/products/kidCollection" onClick={()=>props.setSelect('kidCollection')}>Moda infantil</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/products">Todos los productos</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/femaleCollection" >Moda femenina</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/maleCollection" >Moda Masculina</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/kidCollection" >Moda infantil</NavDropdown.Item>
         </NavDropdown>
       </Nav>
-       <MyAcc />
+      <Nav className="nav-sesion">
+      <Nav.Link as={Link} to ="/login">Inicia Sesión</ Nav.Link>
+      <Nav.Link as={Link} to ="/registro">Únete a Nosotros</ Nav.Link>
+      </Nav>
     </Navbar.Collapse>
   </Navbar>)
 }
