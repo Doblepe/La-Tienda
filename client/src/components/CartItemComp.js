5adcbf7dad661813c75739c882a371f6702aeab8
Doblepe <vicdobleperez@gmail.com>
2021-07-16 19:17:53 +0200

    reddux

--- client/src/components/CartItemComp.js
+++ client/src/components/CartItemComp.js
@@ -1,12 +1,17 @@
 import {Card, Col, Button} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {removeFromCart} from '../../src/redux/shopping/shopping-actions'
 
-function CartItemComp({itemData}){
+function CartItemComp({itemData, removeFromCart}){
     return ( (<Col xs={12} md={4} lg={3}>
         <Card style={{ width: '18rem' }} key={itemData.index}>
         <Card.Img variant="top" src ={itemData.url_img} alt={itemData.title} />
          <Card.Body>
          <Card.Title>{itemData.title}</Card.Title>
          <Card.Text>{itemData.price} EUR. </Card.Text>
+         <Button onClick={()=>removeFromCart(itemData.id)}>
+             Borrar
+         </Button>
          <div>
              <label htmlFor="qty">Qty</label>
              <input min="1" type="number" id="qty" name="qty" value={itemData.qty} />
@@ -16,6 +21,11 @@ function CartItemComp({itemData}){
         </Col>))
 
 }
-export default CartItemComp
+const mapDispatchToProps = dispatch => {
+    return {
+        removeFromCart:(id)=>dispatch(removeFromCart(id))
+    }
+}
+export default connect(null, mapDispatchToProps)(CartItemComp)
 
 
